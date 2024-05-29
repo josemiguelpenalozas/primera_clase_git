52a9e13b5447e5a87720f953bc9a6bcc294091e4
Jose_penaloza <jo.penalozas@duocuc.cl>
2024-05-29 09:23:17 -0400

    crea diccionario del ejercicio

---
+++ ej_git.py
@@ -0,0 +1,8 @@
+#ejercisio diccionario de hoy:usuario:
+diccionario={
+    "identificador":1,
+    "nombre": "Marriott",
+    "direccion":"Avenida bernardo O'higgins 1123",
+    "funcionando":True,
+    "valor habitacion":10000
+}
